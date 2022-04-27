569652130afa49d07b77eaa8e3f548f7d7c588ad
Harrison Wheeler <wheelerharrison93@gmail.com>
2022-04-27 09:51:41 -0600

    initial

---
+++ src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'bcw-harrisonw.us.auth0.com'
+export const audience = 'https://sandbox.com'
+export const clientId = 'VswNnhtKmw8npDG6i67MB2OTyc3HwpcJ'
